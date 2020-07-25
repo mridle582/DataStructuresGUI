0baba7e9083c11dd06a80ad60903bac3d2eff075
mridle582 <mr.idle582@gmail.com>
2020-07-25 18:51:43 -0400

    Message(commit on 'Master')

--- structs/MyLinkedList.py
+++ structs/MyLinkedList.py
@@ -1,7 +1,5 @@
 class MyLinkedList:
-    
-    max_children = 1
-    
+
     def __init__(self, head):
         self.head = self.tail = self.cursor = head
         self.size = 1
@@ -9,38 +7,61 @@ class MyLinkedList:
     def add(self, node):
         if (self.head is None):
             self.head = self.tail = self.cursor = node
-        elif (self.head is not None):
+        else:
             self.tail.children[0] = node
             node.parent = self.tail
             self.tail = node
         self.size += 1
         
-    def insert(self, node, target):
-        cursor = self.head
-        while cursor is not None and cursor.data != target:
-            cursor = cursor.children[0]
-        if cursor is None:
-            print('The target value does not exist.')
-        elif cursor.data == target:
-            if cursor.children[0] is not None:
-                node.children[0] = cursor.children[0]
-                node.parent = cursor
-                cursor.children[0].parent = node
-                cursor.children[0] = node
+    def insert(self, node):
+        if self.cursor is None:
+            self.head = self.cursor = self.tail = node
+        else:
+            if self.cursor.children[0] is not None:
+                node.children[0] = self.cursor.children[0]
+                node.parent = self.cursor
+                self.cursor.children[0].parent = node
+                self.cursor.children[0] = node
             else:
                 self.cursor.children[0] = node
                 node.parent = self.cursor
                 self.tail = node
-        size += 1
+        self.size += 1
 
-    def print(self):
+    def remove(self):
+        if self.head is None:
+            print('The list is empty.')
+        else:
+            old_cursor = self.cursor
+            if self.size == 1:
+                self.head = self.tail = None
+            elif self.cursor is self.head:
+                self.head = self.head.children[0]
+                self.head.parent = None
+            elif self.cursor is self.tail:
+                self.tail = self.tail.parent
+                self.tail.children[0] = None
+            else:
+                self.cursor.parent.children[0] = self.cursor.children[0]
+                self.cursor.children[0].parent = self.cursor.parent
+            if self.cursor.children[0] is not None:
+                self.cursor = self.cursor.children[0]
+            elif self.cursor.parent is not None:
+                self.cursor = self.cursor.parent
+            else:
+                self.cursor = None
+            self.size -= 1
+            return old_cursor
+
+
+    def toString(self):
         output = '['
         cursor = self.head
         while cursor is not None:
             output += '(' + str(cursor.data)  + ')'
             cursor = cursor.children[0]
-        output += ']'
-        print(output)
+        return output + ']'
+        
 
 
 class MyNode:
@@ -51,7 +72,19 @@ class MyNode:
         self.children = [None]
 
 my_list = MyLinkedList(MyNode(5))
-my_list.print()
+print(my_list.toString())
 my_list.add(MyNode(4))
-my_list.print()
+print(my_list.toString())
+my_list.insert(MyNode(3))
+print(my_list.toString())
 print(my_list.cursor.data)
+my_list.remove()
+print(my_list.toString())
+print(my_list.cursor.data)
+my_list.cursor = my_list.cursor.children[0]
+print(my_list.cursor.data)
+my_list.remove()
+print(my_list.toString())
+print(my_list.cursor.data)
+my_list.remove()
+print(my_list.toString())
